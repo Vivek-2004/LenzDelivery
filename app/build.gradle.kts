3e40af3f8f91028992ab4730b332126d3940ebf6
Vivek Ghosh <ghoshvivek004@gmail.com>
2025-03-19 08:12:40 +0530

    LenZ Delivery v1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.fitting.lenzdelivery"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.9"
+        versionCode = 10
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
